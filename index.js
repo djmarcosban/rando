f16e5b0a53ca53c61aa89c6c1c3c5a4d30cbdddb
Marcos Felipe <djmarcosban@hotmail.com>
2020-09-15 14:52:03 -0300

    c

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 const upload = multer({ 
   storage: multer.diskStorage({
-    destination: 'public/modelos/',
+    destination: 'client/build/modelos/',
     filename(req, file, callback) {
       const fileName = `${uuid()}-${file.originalname}`;
 
