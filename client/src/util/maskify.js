e64197c60d94715a447523510b31f6a7fe0a474a
Marcos Felipe <djmarcosban@hotmail.com>
2020-09-16 17:43:24 -0300

    correcao de fotos estarem repetindo

--- client/src/util/maskify.js
+++ client/src/util/maskify.js
@@ -1,18 +1,14 @@
-import * as faceapi from "face-api.js";
+import * as faceapi from "face-api.js"
+import { getEspecificParam } from "./getparams";
 
 const getOverlayValues = landmarks => {
   const nose = landmarks.getNose()
   const jawline = landmarks.getJawOutline()
-
   const jawLeft = jawline[0]
   const jawRight = jawline.splice(-1)[0]
   const adjacent = jawRight.x - jawLeft.x
   const opposite = jawRight.y - jawLeft.y
   const jawLength = Math.sqrt(Math.pow(adjacent, 2) + Math.pow(opposite, 2))
-
-  // Both of these work. The chat believes atan2 is better.
-  // I don't know why. (It doesn’t break if we divide by zero.)
-  // const angle = Math.round(Math.tan(opposite / adjacent) * 100)
   const angle = Math.atan2(opposite, adjacent) * (180 / Math.PI)
   const width = jawLength * 2.2
 
@@ -24,14 +20,48 @@ const getOverlayValues = landmarks => {
   }
 }
 
-const getRandomMask = masks => {
-  const index = Math.floor(masks.length * Math.random())
+function randomGenerator(low, high) {
+  if (arguments.length < 2) {
+    high = low;
+    low = 0;
+  }
+  this.low = low;
+  this.high = high;
+  this.reset();
+}
 
+randomGenerator.prototype = {
+  reset: function() {
+    this.remaining = []
+    for (var i = this.low; i <= this.high; i++) {
+      this.remaining.push(i)
+    }
+  },
+  get: function() {
+    if (!this.remaining.length) {
+      this.reset()
+    }
+    var index = Math.floor(Math.random() * this.remaining.length)
+    var val = this.remaining[index]
+    this.remaining.splice(index, 1)
+    return val
+  }
+}
+
+const oculosParam = getEspecificParam('oculos').split(",")
+const oculosParamLengthLoss = Math.floor(oculosParam.length - 1)
+
+var r = new randomGenerator(0, oculosParamLengthLoss)
+
+const getRandomMask = masks => {
+  //const index = Math.floor(masks.length * Math.random())
+  const index = r.get()
   return masks[index]
 }
 
+
 export async function maskify(masks) {
-  console.log("Maskify starting...")
+  console.log("Debug: analisando")
   await Promise.all([
     faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
     faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
@@ -39,8 +69,6 @@ export async function maskify(masks) {
   ]).catch(error => {
     console.error(error)
   })
-
-  console.log("models loaded")
 
   const items = document.querySelectorAll(".grid-item")
 
@@ -58,10 +86,10 @@ export async function maskify(masks) {
       }
 
       const overlayValues = getOverlayValues(detection.landmarks)
-
       const overlay = document.createElement("img")
+
       overlay.src = getRandomMask(masks)
-      overlay.alt = "mask overlay."
+      overlay.alt = ""
       overlay.style.cssText = `
         position: absolute;
         left: ${overlayValues.leftOffset * scale}px;
@@ -69,14 +97,12 @@ export async function maskify(masks) {
         width: ${overlayValues.width * scale}px;
         transform: rotate(${overlayValues.angle}deg);
       `
-
       item.appendChild(overlay)
-      console.log("Maskify finished...")
-
+      console.log('Debug: renderizado')
     }
-    
-    // To avoid CORS issues we create a cross-origin-friendly copy of the image.
+
     const image = new Image()
+
     image.crossOrigin = "Anonymous"
     image.addEventListener("load", handleImage(originalImage, image))
     image.src = originalImage.src
